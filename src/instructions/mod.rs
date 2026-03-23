07c9281880f7533f8c9488a830aa959a386fd66d
subh <subhajitchaudhury05@gmail.com>
2026-03-23 05:54:07 +0530

    feat : add empty instruction files

--- src/instructions/mod.rs
+++ src/instructions/mod.rs
@@ -1,9 +1,1 @@
-pub mod create_mint;
-pub mod mint;
-pub mod token_account;
-pub mod transfer;
 
-pub use create_mint::*;
-pub use mint::*;
-pub use token_account::*;
-pub use transfer::*;
---
+++ src/instructions/transfer.rs
@@ -0,0 +1,1 @@
+use quasar_lang::prelude::*;
