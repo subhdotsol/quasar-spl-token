81a98a2af0b48df758f0e96d5973b493e2e0d294
subh <subhajitchaudhury05@gmail.com>
2026-03-23 05:53:46 +0530

    feat : add project skeleton in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,10 @@
 #![cfg_attr(not(test), no_std)]
 
 use quasar_lang::prelude::*;
-mod instructions;
-use instructions::*;
 
 declare_id!("7oY3XcwXGnonxNs92FrnR7e1Dtvf8pRLExvDgznzWTyU");
-
-#[derive(Accounts)]
-pub struct Initialize<'info> {
-    pub payer: &'info mut Signer,
-    pub system_program: &'info Program<System>,
-}
-
-impl<'info> Initialize<'info> {
-    #[inline(always)]
-    pub fn initialize(&self) -> Result<(), ProgramError> {
-        Ok(())
-    }
-}
 
 #[program]
 mod quasar_spl_token {
     use super::*;
-
-    #[instruction(discriminator = 0)]
-    pub fn initialize(ctx: Ctx<Initialize>) -> Result<(), ProgramError> {
-        ctx.accounts.initialize()
-    }
 }
-
-#[cfg(test)]
-mod tests;
