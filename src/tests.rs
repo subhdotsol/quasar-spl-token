871158ea32a64999cb666966722aecd53d092209
subh <subhajitchaudhury05@gmail.com>
2026-03-23 05:56:47 +0530

    feat : add test suite skeleton

--- src/tests.rs
+++ src/tests.rs
@@ -1,38 +1,33 @@
 extern crate std;
 
-use quasar_svm::{Account, Instruction, Pubkey, QuasarSvm};
+use quasar_svm::{
+    token::{self, Mint, TokenAccount},
+    Account, Instruction, Pubkey, QuasarSvm,
+};
 use solana_address::Address;
+use solana_program_pack::Pack;
 
-use quasar_spl_token_client::InitializeInstruction;
+use quasar_spl_token_client::{
+    CreateMintInstruction, CreateTokenAccountInstruction, MintTokensInstruction,
+    TransferInstruction,
+};
+
+const TOKEN_PROGRAM_ID: Pubkey =
+    solana_pubkey::pubkey!("TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA");
 
 fn setup() -> QuasarSvm {
     let elf = include_bytes!("../target/deploy/quasar_spl_token.so");
-    QuasarSvm::new()
-        .with_program(&Pubkey::from(crate::ID), elf)
+    QuasarSvm::new().with_program(&Pubkey::from(crate::ID), elf)
 }
 
-#[test]
-fn test_initialize() {
-    let mut svm = setup();
+fn to_address(pubkey: &Pubkey) -> Address {
+    Address::from(pubkey.to_bytes())
+}
 
-    let payer = Pubkey::new_unique();
-
-    let instruction: Instruction = InitializeInstruction {
-        payer: Address::from(payer.to_bytes()),
-        system_program: Address::from(quasar_svm::system_program::ID.to_bytes()),
+/// Mark specific account indices as signers on an instruction.
+fn with_signers(mut ix: Instruction, indices: &[usize]) -> Instruction {
+    for &i in indices {
+        ix.accounts[i].is_signer = true;
     }
-    .into();
-
-    let result = svm.process_instruction(
-        &instruction,
-        &[Account {
-            address: payer,
-            lamports: 10_000_000_000,
-            data: vec![],
-            owner: quasar_svm::system_program::ID,
-            executable: false,
-        }],
-    );
-
-    result.assert_success();
+    ix
 }
