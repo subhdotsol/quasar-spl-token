7ba5634d1404a2a6a5cd373b1a71d4f2277a840d
subh <subhajitchaudhury05@gmail.com>
2026-03-23 05:54:32 +0530

    feat : add CreateMint account struct

--- src/instructions/create_mint.rs
+++ src/instructions/create_mint.rs
@@ -1,1 +1,18 @@
 use quasar_lang::prelude::*;
+use quasar_spl::{Mint, Token};
+
+#[derive(Accounts)]
+pub struct CreateMint<'info> {
+    pub payer: &'info mut Signer,
+    #[account(
+        init,
+        payer = payer,
+        mint::decimals = 6,
+        mint::authority = payer,
+        seeds = [b"mint", payer],
+        bump,
+    )]
+    pub mint: &'info mut Account<Mint>,
+    pub token_program: &'info Program<Token>,
+    pub system_program: &'info Program<System>,
+}
