d6b7c153cc81f312283f61bc8ae6b9cf5e31b4ec
subh <subhajitchaudhury05@gmail.com>
2026-03-23 05:54:49 +0530

    feat : add MintTokens account struct

--- src/instructions/mint.rs
+++ src/instructions/mint.rs
@@ -1,1 +1,12 @@
 use quasar_lang::prelude::*;
+use quasar_spl::{Mint, Token};
+
+#[derive(Accounts)]
+pub struct MintTokens<'info> {
+    pub authority: &'info mut Signer,
+    #[account(mut)]
+    pub mint: &'info mut Account<Mint>,
+    #[account(mut)]
+    pub to: &'info mut Account<Token>,
+    pub token_program: &'info Program<Token>,
+}
