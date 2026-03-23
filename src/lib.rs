9f0b28eb655004d695a9f4f71175aecbcd28653a
subh <subhajitchaudhury05@gmail.com>
2026-03-23 05:56:29 +0530

    feat : implement program entrypoints in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -9,4 +9,27 @@ declare_id!("7oY3XcwXGnonxNs92FrnR7e1Dtvf8pRLExvDgznzWTyU");
 #[program]
 mod quasar_spl_token {
     use super::*;
+
+    #[instruction(discriminator = 0)]
+    pub fn create_mint(ctx: Ctx<CreateMint>) -> Result<(), ProgramError> {
+        ctx.accounts.create_mint()
+    }
+
+    #[instruction(discriminator = 1)]
+    pub fn mint_tokens(ctx: Ctx<MintTokens>, amount: u64) -> Result<(), ProgramError> {
+        ctx.accounts.mint_tokens(amount)
+    }
+
+    #[instruction(discriminator = 2)]
+    pub fn create_token_account(ctx: Ctx<CreateTokenAccount>) -> Result<(), ProgramError> {
+        ctx.accounts.create_token_account()
+    }
+
+    #[instruction(discriminator = 3)]
+    pub fn transfer(ctx: Ctx<TransferTokens>, amount: u64) -> Result<(), ProgramError> {
+        ctx.accounts.transfer_tokens(amount)
+    }
 }
+
+#[cfg(test)]
+mod tests;
