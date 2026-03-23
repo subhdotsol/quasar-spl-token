8a0f96885c40ceb361a74c93f679cd9b8625cb5b
subh <subhajitchaudhury05@gmail.com>
2026-03-23 05:55:34 +0530

    feat : add TransferTokens account struct

--- src/instructions/transfer.rs
+++ src/instructions/transfer.rs
@@ -1,1 +1,12 @@
 use quasar_lang::prelude::*;
+use quasar_spl::Token;
+
+#[derive(Accounts)]
+pub struct TransferTokens<'info> {
+    pub authority: &'info mut Signer,
+    #[account(mut)]
+    pub from: &'info mut Account<Token>,
+    #[account(mut)]
+    pub to: &'info mut Account<Token>,
+    pub token_program: &'info Program<Token>,
+}
