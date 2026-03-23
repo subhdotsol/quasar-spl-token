25bfe047354bdbf29491a321c4acdbb02497de3a
subh <subhajitchaudhury05@gmail.com>
2026-03-23 05:55:06 +0530

    feat : add CreateTokenAccount account struct

--- src/instructions/token_account.rs
+++ src/instructions/token_account.rs
@@ -1,1 +1,19 @@
 use quasar_lang::prelude::*;
+use quasar_spl::{Mint, Token};
+
+#[derive(Accounts)]
+pub struct CreateTokenAccount<'info> {
+    pub payer: &'info mut Signer,
+    pub owner: &'info mut UncheckedAccount,
+    pub mint: &'info Account<Mint>,
+    #[account(
+        init,
+        payer = payer,
+        token::mint = mint,
+        token::authority = owner,
+    )]
+    pub token_account: &'info mut Account<Token>,
+    pub rent: &'info Sysvar<Rent>,
+    pub token_program: &'info Program<Token>,
+    pub system_program: &'info Program<System>,
+}
